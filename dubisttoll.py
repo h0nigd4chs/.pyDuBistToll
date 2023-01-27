544b2dcce3cd382e6d1c20627dad59df04d459a9
h0nigd4chs <123739154+h0nigd4chs@users.noreply.github.com>
2023-01-27 13:06:06 +0100

    Create dubisttoll.py

---
+++ dubisttoll.py
@@ -0,0 +1,10 @@
+#Das Skript soll eine Nachricht anzeigen, die DICH daran erinnert, wie toll DU bist
+
+import time 
+
+#eine Schleife, die jeden Tag aufgerufen wird
+while True:
+    print("Du bist toll! Vergiss nicht, das jeden Tag zu glauben!")
+
+    #ein Timer, der alle 24 Stunden aufgerufen wird
+    time.sleep(86400)
